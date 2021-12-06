ad2eacc7fad889395db2c3dd386d5130149498c0
Martin Michalek <martin.michalek@goodrequest.com>
2021-12-06 21:14:13 +0100

    Day 5 in progress

--- day5/day05.py
+++ day5/day05.py
@@ -7,49 +7,60 @@ data = [i[:-1].replace(" -> ", ",") for i in file]
 def point_grid(position, idx, start, stop, grid):
     if start > stop:
         start, stop = stop, start
-
     for i in range(start, stop + 1):
         if position:
-            grid[i][idx] += 1
-        else:
             grid[idx][i] += 1
+        else:
+            grid[i][idx] += 1
 
     return grid
 
 
 def func1():
-    tmp = [''.join(i).split(",") for i in data]
-
-    x1, x2 = [], []
-    y1, y2 = [], []
-
-    for value in tmp:
-        for i in range(len(value)):
-            position = i % 4
-            if position == 0:
-                x1.append(int(value[i]))
-            elif position == 1:
-                y1.append(int(value[i]))
-            elif position == 2:
-                x2.append(int(value[i]))
-            else:
-                y2.append(int(value[i]))
-
+    all_data = [''.join(i).split(",") for i in data]
     grid = [[0 for _ in range(1000)] for _ in range(1000)]
+    test = {}
 
-    for i in range(len(x1)):
-        x_1, x_2 = int(x1[i]), int(x2[i])
-        y_1, y_2 = int(y1[i]), int(y2[i])
-
+    all = 0
+    for x_1, y_1, x_2, y_2 in [[int(y) for y in x] for x in all_data]:
         if x_1 == x_2:
-            grid = point_grid(True, x_1, y_1, y_2, grid)
+            if y_1 > y_2: y_1, y_2 = y_2, y_1
+            for y in range(y_1, y_2 + 1):
+                coordinates = (x_1, y)
+                if coordinates in test:
+                    test[coordinates] += 1
+                else:
+                    test[coordinates] = 1
+
+            # point_grid(True, x_1, y_1, y_2, grid)
         elif y_1 == y_2:
-            grid = point_grid(False, y_1, x_1, x_2, grid)
+            if x_1 > x_2: x_1, x_2 = x_2, x_1
+            for x in range(x_1, x_2 + 1):
+                coordinates = (x, y_2)
+                if coordinates in test:
+                    test[coordinates] += 1
+                else:
+                    test[coordinates] = 1
+            # point_grid(False, y_1, x_1, x_2, grid)
         else:
-            continue
+            all += 1
+
+    counter = 0
+    for value in test.values():
+        if value >= 2:
+            counter += 1
+    print(counter, all)
 
     np_array = np.array(grid)
-    print(np.count_nonzero(np_array == 2))  # 4822, 6283
+    print(np.count_nonzero(np_array >= 2))
+
+    val = 0
+    for x in grid:
+        for k in x:
+            if k >= 2:
+                val += 1
+
+    print(val)
 
 
 def func2():
@@ -57,5 +68,5 @@ def func2():
 
 
 if __name__ == "__main__":
-    func1()
+    func1()  # 4822, 6283
     # func2() # 18864
