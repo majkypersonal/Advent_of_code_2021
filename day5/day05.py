dd9cd0d0191aeb0276382e959c8b4cbc43aa68a0
martin-michalek <52975119+majkypersonal@users.noreply.github.com>
2021-12-07 13:02:35 +0100

    Day 5 Completely!!

--- day5/day05.py
+++ day5/day05.py
@@ -4,24 +4,10 @@ file = open('input05', "r+").readlines()
 data = [i[:-1].replace(" -> ", ",") for i in file]
 
 
-def point_grid(position, idx, start, stop, grid):
-    if start > stop:
-        start, stop = stop, start
-    for i in range(start, stop + 1):
-        if position:
-            grid[idx][i] += 1
-        else:
-            grid[i][idx] += 1
-
-    return grid
-
-
 def func1():
     all_data = [''.join(i).split(",") for i in data]
-    grid = [[0 for _ in range(1000)] for _ in range(1000)]
     test = {}
 
-    all = 0
     for x_1, y_1, x_2, y_2 in [[int(y) for y in x] for x in all_data]:
         if x_1 == x_2:
             if y_1 > y_2: y_1, y_2 = y_2, y_1
@@ -31,8 +17,6 @@ def func1():
                     test[coordinates] += 1
                 else:
                     test[coordinates] = 1
-
-            # point_grid(True, x_1, y_1, y_2, grid)
         elif y_1 == y_2:
             if x_1 > x_2: x_1, x_2 = x_2, x_1
             for x in range(x_1, x_2 + 1):
@@ -41,32 +25,64 @@ def func1():
                     test[coordinates] += 1
                 else:
                     test[coordinates] = 1
-            # point_grid(False, y_1, x_1, x_2, grid)
-        else:
-            all += 1
 
     counter = 0
     for value in test.values():
         if value >= 2:
             counter += 1
-    print(counter, all)
-
-    np_array = np.array(grid)
-    print(np.count_nonzero(np_array >= 2))
-
-    val = 0
-    for x in grid:
-        for k in x:
-            if k >= 2:
-                val += 1
-
-    print(val)
+    print(counter)
 
 
 def func2():
-    print("Func2")
+    all_data = [''.join(i).split(",") for i in data]
+    test = {}
+
+    for x_1, y_1, x_2, y_2 in [[int(y) for y in x] for x in all_data]:
+        if x_1 == x_2:
+            if y_1 > y_2:
+                y_1, y_2 = y_2, y_1
+
+            for y in range(y_1, y_2 + 1):
+                coordinates = (x_1, y)
+                if coordinates in test:
+                    test[coordinates] += 1
+                else:
+                    test[coordinates] = 1
+        elif y_1 == y_2:
+            if x_1 > x_2:
+                x_1, x_2 = x_2, x_1
+
+            for x in range(x_1, x_2 + 1):
+                coordinates = (x, y_2)
+                if coordinates in test:
+                    test[coordinates] += 1
+                else:
+                    test[coordinates] = 1
+        else:
+            if x_1 < x_2:
+                range_x = list(range(x_1, x_2 + 1))
+            else:
+                range_x = list(range(x_1, x_2 - 1, -1))
+
+            if y_1 < y_2:
+                range_y = list(range(y_1, y_2 + 2))
+            else:
+                range_y = list(range(y_1, y_2 - 1, -1))
+
+            for i in range(len(range_x)):
+                coordinates = (range_x[i], range_y[i])
+                if coordinates in test:
+                    test[coordinates] += 1
+                else:
+                    test[coordinates] = 1
+
+    counter = 0
+    for value in test.values():
+        if value >= 2:
+            counter += 1
+    print(counter)
 
 
 if __name__ == "__main__":
-    func1()  # 4822, 6283
-    # func2() # 18864
+    # func1()  # 5145
+    func2()  # 18864
