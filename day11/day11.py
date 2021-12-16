289eae9ec9aa58f06e78421f3e8a21107a2063fe
martin-michalek <52975119+majkypersonal@users.noreply.github.com>
2021-12-16 13:08:18 +0100

    Day 11 DONE EASY!

--- day11/day11.py
+++ day11/day11.py
@@ -6,47 +6,127 @@ def func1():
     grid = [list(map(int, line)) for line in data]
     shines = 0
 
-    for _ in range(100):
+    for step in range(1000000):
         grid = [[val + 1 for val in x] for x in grid]
+        grid_flashes = {}
+        need_check = {}
 
-        need_check = 0
-
-        if need_check > 0:
-            for i in range(len(grid)):
-                for j in grid[i]:
-                    if grid[i][j] > 9:
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                if grid[i][j] > 9:
+                    grid[i][j] = 0
+                    if (i, j) not in grid_flashes:
                         shines += 1
-                        grid[i][j] = 0
-                        
-                        # DOWN
-                        if 0 <= i + 1 < len(grid):
-                            grid[i + 1][j] += 1
-                            if grid[i + 1][j] > 9:
-                                need_check += 1
+                        grid_flashes[(i, j)] = 1
 
-                            if 0 <= j - 1 < len(grid[i + 1]):
-                                grid[i + 1][j - 1] += 1
-                                if grid[i + 1][j - 1] > 9:
-                                    need_check += 1
-                            elif 0 <= j + 1 < len(grid[i + 1]):
-                                grid[i + 1][j] += 1
-                                if grid[i + 1][j] > 9: need_check += 1
-                        # UP
-                        if 0 <= i - 1 < len(grid):
-                            grid[i - 1][j] += 1
-                            if grid[i + 1][j] > 9: need_check += 1
-                        if 0 <= j + 1 < len(grid[i]):
-                            grid[i + 1][j] += 1
-                            if grid[i + 1][j] > 9: need_check += 1
-                        if 0 <= j - 1 < len(grid[i]):
-                            grid[i + 1][j] += 1
-                            if grid[i + 1][j] > 9: need_check += 1
+                    if (i, j) in need_check:
+                        need_check.pop((i, j))
+
+                    # DOWN
+                    if 0 <= i + 1 < len(grid):
+                        grid[i + 1][j] += 1
+                        if grid[i + 1][j] > 9 and (i + 1, j) not in need_check:
+                            need_check[(i + 1, j)] = 1
+
+                        if 0 <= j - 1 < len(grid[i + 1]):
+                            grid[i + 1][j - 1] += 1
+                            if grid[i + 1][j - 1] > 9 and (i + 1, j - 1) not in need_check:
+                                need_check[(i + 1, j - 1)] = 1
+                        if 0 <= j + 1 < len(grid[i + 1]):
+                            grid[i + 1][j + 1] += 1
+                            if grid[i + 1][j + 1] > 9 and (i + 1, j + 1) not in need_check:
+                                need_check[(i + 1, j + 1)] = 1
+
+                    # UP
+                    if 0 <= i - 1 < len(grid):
+                        grid[i - 1][j] += 1
+                        if grid[i - 1][j] > 9 and (i - 1, j) not in need_check:
+                            need_check[(i - 1, j)] = 1
+
+                        if 0 <= j - 1 < len(grid[i - 1]):
+                            grid[i - 1][j - 1] += 1
+                            if grid[i - 1][j - 1] > 9 and (i - 1, j - 1) not in need_check:
+                                need_check[(i - 1, j - 1)] = 1
+                        if 0 <= j + 1 < len(grid[i - 1]):
+                            grid[i - 1][j + 1] += 1
+                            if grid[i - 1][j + 1] > 9 and (i - 1, j + 1) not in need_check:
+                                need_check[(i - 1, j + 1)] = 1
+
+                    # RIGHT
+                    if 0 <= j + 1 < len(grid[i]):
+                        grid[i][j + 1] += 1
+                        if grid[i][j + 1] > 9 and (i, j + 1) not in need_check:
+                            need_check[(i, j + 1)] = 1
+
+                    # LEFT
+                    if 0 <= j - 1 < len(grid[i]):
+                        grid[i][j - 1] += 1
+                        if grid[i][j - 1] > 9 and (i, j - 1) not in need_check:
+                            need_check[(i, j - 1)] = 1
+
+        grid, shines, grid_flashes = check_surrounding(grid, need_check, grid_flashes, shines)
+        for i, j in grid_flashes:
+            grid[i][j] = 0
+
+        if sum(sum(grid, [])) == 0:
+            print(step)
+
+    print("Shines: {}".format(shines))
 
 
-def func2():
-    print("Func2")
+def check_surrounding(grid, need_check, grid_flashes, shines):
+    while len(need_check) > 0:
+        i, j = need_check.popitem()[0]
+        grid[i][j] = 0
+        if (i, j) not in grid_flashes:
+            shines += 1
+            grid_flashes[(i, j)] = 1
+
+        # DOWN
+        if 0 <= i + 1 < len(grid):
+            grid[i + 1][j] += 1
+            if grid[i + 1][j] > 9 and (i + 1, j) not in need_check:
+                need_check[(i + 1, j)] = 1
+
+            if 0 <= j - 1 < len(grid[i + 1]):
+                grid[i + 1][j - 1] += 1
+                if grid[i + 1][j - 1] > 9 and (i + 1, j - 1) not in need_check:
+                    need_check[(i + 1, j - 1)] = 1
+            if 0 <= j + 1 < len(grid[i + 1]):
+                grid[i + 1][j + 1] += 1
+                if grid[i + 1][j + 1] > 9 and (i + 1, j + 1) not in need_check:
+                    need_check[(i + 1, j + 1)] = 1
+
+        # UP
+        if 0 <= i - 1 < len(grid):
+            grid[i - 1][j] += 1
+            if grid[i - 1][j] > 9 and (i - 1, j) not in need_check:
+                need_check[(i - 1, j)] = 1
+
+            if 0 <= j - 1 < len(grid[i - 1]):
+                grid[i - 1][j - 1] += 1
+                if grid[i - 1][j - 1] > 9 and (i - 1, j - 1) not in need_check:
+                    need_check[(i - 1, j - 1)] = 1
+            if 0 <= j + 1 < len(grid[i - 1]):
+                grid[i - 1][j + 1] += 1
+                if grid[i - 1][j + 1] > 9 and (i - 1, j + 1) not in need_check:
+                    need_check[(i - 1, j + 1)] = 1
+
+        # RIGHT
+        if 0 <= j + 1 < len(grid[i]):
+            grid[i][j + 1] += 1
+            if grid[i][j + 1] > 9 and (i, j + 1) not in need_check:
+                need_check[(i, j + 1)] = 1
+
+        # LEFT
+        if 0 <= j - 1 < len(grid[i]):
+            grid[i][j - 1] += 1
+            if grid[i][j - 1] > 9 and (i, j - 1) not in need_check:
+                need_check[(i, j - 1)] = 1
+
+    return grid, shines, grid_flashes
 
 
 if __name__ == "__main__":
-    func1()
+    print(func1())
     # func2()
